6fc8542156e432a011db33154a58ca80c8f1070a
chelo.chel <122210462+chwello@users.noreply.github.com>
2023-11-24 23:21:16 +0800

    Add files via upload
    tailwind

---
+++ script.js
@@ -0,0 +1,41 @@
+
+//source code w3school HAHHAHAHAHA
+function menu(evt, classes) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+
+  
+  document.getElementById(classes).style.display = "block";
+
+  evt.target.classList.add("active");
+}
+
+
+function hideAllTabContents() {
+  var tabcontents = document.getElementsByClassName("tabcontent");
+  for (var i = 0; i < tabcontents.length; i++) {
+    if (tabcontents[i].id !== "Dish") {
+      tabcontents[i].style.display = "none";
+    }
+  }
+  var defaultButton = document.querySelector(".tablinks[data-default='true']");
+  defaultButton.classList.add("active");
+}
+
+
+
+window.onload = function() {
+  hideAllTabContents();
+};
+
+
